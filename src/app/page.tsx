cace0a3c57b3c7abcf1a2c4d02979278df094cbf
codedusting <codedusting@gmail.com>
2023-10-22 14:27:30 +0530

    NextJS App Router Minimal Starter Template

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+	return (
+		<section className="mt-48 flex flex-col items-center justify-center">
+			<h1 className="text-6xl font-bold">Welcome to NextJS Starter</h1>
+		</section>
+	);
+};
+
+export default Home;
